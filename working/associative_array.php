416bb2dc199be5e43cc6b52e59c6a6a35b7d2927
mattrijk <mattrijk11@gmail.com>
2016-07-04 00:04:30 +0000

    added arrays file to repo.

---
+++ working/associative_array.php
@@ -0,0 +1,16 @@
+<?php 
+// working with Associative Arrays
+
+// associative array use a string as a key
+$name['first'] = 'Julia';
+$name['last'] = 'Roberts';
+$name['age'] = 35;
+$name['occupation'] = 'Actress';
+
+
+
+// you can then access the elements by key
+printf('<br /> Full Name: %s %s', $name['first'], $name['last']);
+
+
+?>
